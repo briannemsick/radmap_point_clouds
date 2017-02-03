34f165182ecd17867158e05fa3305b38947af86b
briannemsick <brian.nemsick@gmail.com>
2017-02-03 12:25:17 -0800

    Cartographer without IMU

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # Merging the bags
-1. Place port.bag, starboard.bag, imu.bag into a folder
-2. roslaunch radmap_preprocess lasers.launch 
-3. rosrun  rosrun radmap_preprocess lasers 
-4. python [insert path]/src/bag_merge_radmap.py
-
-# Coordinate frame calibration
-1. velodyne_calib.m
-2. fill in calibrated_tf.launch
+1. Place port.bag, starboard.bag, imu.bag into [insert folder]
+2. cd [insert folder]
+3. roslaunch radmap_preprocess lasers.launch 
+4. rosrun  rosrun radmap_preprocess lasers 
+5. python [insert path]/src/bag_merge_radmap.py
 
 # Play the bags
-1. rosparam set use_sim_time true
-2. roslaunch radmap_preprocess calibrated_tf.launch
-3. rosbag play -- clock 
-4. rviz
+1. roslaunch radmap_preprocess calibrated_tf.launch
+2. rosbag play [insert bagfile]
+3. rviz
+
+# Run Cartographer
+roslaunch cartographer_ros demo_radmap.launch bag_filename:=[insert bagfile path]
+
+# Coordinate frame calibration (already completed)
+1. velodyne_calib.m
+2. fill in calibrated_tf.launch
+3. roslaunch radmap_preprocess calibrated_tf.launch
+4. rosrun tf tf_echo imu_link velodyne_port --> radmap.lau::velodyne_port_joint::origin
+5. rosrun tf tf_echo imu_link velodyne_starboard --> radmap.lau::velodyne_starboard_joint::origin
--- cartographer_configs/radmap.lua
+++ cartographer_configs/radmap.lua
@@ -17,8 +17,8 @@ include "map_builder.lua"
 options = {
   map_builder = MAP_BUILDER,
   map_frame = "map",
-  tracking_frame = "imu_link",
-  published_frame = "imu_link",
+  tracking_frame = "base_link",
+  published_frame = "base_link",
   odom_frame = "odom",
   provide_odom_frame = false,
   use_odometry = false,
@@ -30,7 +30,7 @@ options = {
   pose_publish_period_sec = 5e-3,
 }
 
-TRAJECTORY_BUILDER_3D.scans_per_accumulation = 160
+TRAJECTORY_BUILDER_3D.scans_per_accumulation = 2
 
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 7
@@ -38,6 +38,9 @@ MAP_BUILDER.sparse_pose_graph.optimization_problem.huber_scale = 5e2
 MAP_BUILDER.sparse_pose_graph.optimize_every_n_scans = 320
 MAP_BUILDER.sparse_pose_graph.constraint_builder.sampling_ratio = 0.03
 MAP_BUILDER.sparse_pose_graph.optimization_problem.ceres_solver_options.max_num_iterations = 10
+-- The IMU is shitting the bed
+MAP_BUILDER.sparse_pose_graph.optimization_problem.rotation_weight = 3e-3
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 2e-3
 -- Reuse the coarser 3D voxel filter to speed up the computation of loop closure
 -- constraints.
 MAP_BUILDER.sparse_pose_graph.constraint_builder.adaptive_voxel_filter = TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter
